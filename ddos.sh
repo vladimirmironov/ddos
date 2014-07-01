7e9394ba02e74ee2367661280fa8c3417b077168
Vladimir Mironov <vladimir_mironov@epam.com>
2014-07-01 19:00:17 +0400

    first commit

---
+++ ddos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+netstat -ntu | awk '{split($5, a, ":"); print a[1];}' | grep '[[:digit:]{1,3}\.[:digit:]{1,3}\.[:digit:]{1,3}\.[:digit:]{1,3}]' | sort | uniq -c | sort -n | awk '{if ($1 > 2) {print "/sbin/iptables -I INPUT -p tcp --dport 80 -s " $2 " -j DROP"}}'
+
